9c78f872b9d19cb519fef4ca6488ce9032beaa54
joselle4 <joselle4@gmail.com>
2016-08-06 00:04:46 -0400

    modified README and CodeBook. Added tidy data sets: activitySubjectMeasurementMeans.txt and measurementsMeansStDev.txt.  Added run_analysis.R script.

---
+++ README.md
@@ -0,0 +1,62 @@
+==============================
+Getting and Cleaning Data Course Project
+Data Science Specialization
+Coursera
+
+GitHub repository: 
+==============================
+[link here]
+
+Description:
+==============================
+This course project uses the Human Activity Recognition Using Smartphones Dataset (Version 1.0) in order to to demonstrate my ability to collect, work with, and clean a data set. The goal is to prepare tidy data by merging the training and test data sets to use for analysis such as extracting the mean and standard deviation of different measurements.
+
+The data for this project is obtained from: 
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+The project involves creating one R script called run_analysis.R that does the following:
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+This repo includes the following files:
+==============================
+- ‘README.md’
+
+- ‘Codebook.md’: describes the variables, the data, and any transformations or work performed to clean up the data.
+
+- ‘run_analysis.R’: the script performs the following actions:
+
+- ‘measurementsMeanStdDev.txt’: data output from run_analysis.R providing the means and standard deviations of all the measurements from training and test set.  This reflects Step 2 in the description above.
+
+- ‘activitySubjectMeasurementMeans.txt’: data output from run_analysis.R providing the means by measurement, activity and subject.  
+
+Steps of ‘run_analysis.R’:
+==============================
+1. Load the following libraries: dtplyr, plyr, dplyr, data.table, stringr, stats
+2. Set directories and file paths
+3. Read the following files as data tables: 
+- 'features.txt': List of all features.
+- 'activity_labels.txt': Links the class labels with their activity name.
+- 'train/subject_train.txt': Training subjects.
+- 'train/X_train.txt': Training set; loaded using features as column names.
+- 'train/y_train.txt': Training labels.
+- 'test/subject_test.txt': Test subjects.
+- 'test/X_test.txt': Test set; loaded using features as column names.
+- 'test/y_test.txt': Test labels.
+4. Merge activity labels to the y_train and y_test data tables 
+5. Add group column to classify between the train or test data
+6. Combine subject, activity and x tables for train and test data
+7. Merge test and train data
+8. Extract only measurements containing mean and standard deviation. Include the following columns in the data extraction: subject, activityID, activity, group.
+9. Export table as the first tidy data (‘measurementsMeanStdDev.txt’).
+10. Extract mean measurements from the first tidy data and analyze the average measurements by activity and by subject.
+11. Export table as the second tidy data (‘activitySubjectMeasurementMeans.txt’).
+
+License:
+========
+The dataset used for this course project acknowledges the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
---
+++ run_analysis.R
@@ -0,0 +1,73 @@
+# load required libraries
+library(dtplyr)
+library(plyr)
+library(dplyr)
+library(data.table)
+library(stringr)
+library(stats)
+
+# set directories and filepaths:
+dirTidyData <- "/Users/joselle4/Documents/Programming/R/coursera/TidyData"
+dirRawData <- "/Users/joselle4/Documents/Programming/R/coursera/UCI HAR Dataset"
+fileActivityLabels <- paste0(dirRawData, "/activity_labels.txt")
+fileFeatures <- paste0(dirRawData, "/features.txt") 
+fileSubjectTest <- paste0(dirRawData, "/test/subject_test.txt")
+fileXTest <- paste0(dirRawData, "/test/X_test.txt")
+fileYTest <- paste0(dirRawData, "/test/y_test.txt")
+fileSubjectTrain <- paste0(dirRawData, "/train/subject_train.txt")
+fileXTrain <- paste0(dirRawData, "/train/X_train.txt")
+fileYTrain <- paste0(dirRawData, "/train/y_train.txt")
+
+# load/read text files
+## features will serve as the column naames for the "x-files"
+## using features as column names will saitisfy step 4 of the course project:
+## appropriately label the date set with descriptive variable names
+dtFeatures <- read.table(fileFeatures, col.names = c("featuresID", "features"))
+dtActivityLabels <- read.table(fileActivityLabels, 
+                               col.names = c("activityID", "activity"))
+dtSubjectTest <- read.table(fileSubjectTest, col.names = c("subject"))
+dtXTest <- read.table(fileXTest, col.names = dtFeatures$features)
+dtYTest <- read.table(fileYTest, col.names = c("activityID"))
+dtSubjectTrain <- read.table(fileSubjectTrain, col.names = c("subject"))
+dtXTrain <- read.table(fileXTrain, col.names = dtFeatures$features)
+dtYTrain <- read.table(fileYTrain, col.names = c("activityID"))
+
+# before merging the test and training sets, 
+## relabel activities by merging it with the activity labels
+## this would satisfy step 3 of the course project: 
+## use descriptive activity names to name the activities
+dtYTrain <- merge.data.frame(dtActivityLabels, dtYTrain, 
+                             all = TRUE, by = "activityID")
+dtYTest <- merge.data.frame(dtActivityLabels, dtYTest, 
+                            all = TRUE, by = "activityID")
+## add a new column to differentiate between test and training sets
+dtYTest$group <- "test"
+dtYTrain$group <- "train"
+## column bind subjects, y and x for test and training sets
+dtTest <- cbind(dtSubjectTest, dtYTest, dtXTest)
+dtTrain <- cbind(dtSubjectTrain, dtYTrain, dtXTrain)
+
+## merge test and training set via rowbind
+## this would satisfy step 1 of the course project:
+## merge the training and test sets to create one data set
+dtMergeTestTrain <- rbind(dtTest, dtTrain)
+
+# for step 2 of the course project:
+# extract only the measurements on the mean 
+# and standard deviation for each measurement
+dtMeanStDev <- dtMergeTestTrain[ , grepl("subject|activity|group|mean|std", 
+                                         colnames(dtMergeTestTrain))]
+write.table(dtMeanStDev, file = paste0(dirTidyData, 
+                                       "/measurementsMeanStdDev.txt"))
+
+# for step 5 of the course project:
+# create a second tidy data with the average of each 
+# variable and each activity and each subject
+dtMean <- dtMeanStDev[, grepl("subject|activity|mean", 
+                              colnames(dtMeanStDev))]
+dtMeanbyActSub <- aggregate(dtMean[,(4:49)], 
+                           list(dtMean$subject, dtMean$activity), 
+                           FUN = mean)
+dtMeanbyActSub <- rename(dtMeanbyActSub, subject = Group.1, activity = Group.2)
+write.table(dtMeanbyActSub, file = paste0(dirTidyData, 
+                                          "/activitySubjectMeasurementMeans.txt"))
